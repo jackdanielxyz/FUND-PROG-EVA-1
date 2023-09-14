31f2e1b5cfeaf3a5d0f37678fbf6a52d691ac7d1
Dany Perez (Ado's Lover) <143568493+jackdanielxyz@users.noreply.github.com>
2023-09-14 08:16:48 -0600

    Add files via upload

---
+++ EVA1_10_IDENTIFICADORES/src/eva1_10_identificadores/EVA1_10_IDENTIFICADORES.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_10_identificadores;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_10_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int 1salario; no cumple las reglas de id's, comienza con numero
+        //int sal&ario; & no es c. alfanumerico
+        int mi_salario;
+        int $salario;
+        int salario1000;
+        //int _; no
+        int $; //mala variable pero valida
+        //int mi salario; tiene espacio, no bueno
+        int salario;
+        int Salario;
+        int SALARIO;
+        //int salario; var is already defined in method main(String)
+        //int public; no valido, public es keyword
+        
+    }
+    
+}
