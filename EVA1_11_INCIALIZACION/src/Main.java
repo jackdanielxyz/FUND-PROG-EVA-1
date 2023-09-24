c55cd5803d67d38445a2090e9cea7eab9aedf1c7
Dany Perez (Ado's Lover) <143568493+jackdanielxyz@users.noreply.github.com>
2023-09-23 18:41:45 -0600

    Add files via upload

---
+++ EVA1_11_INCIALIZACION/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        parentesis denota funcion
+        print1() <——— funcion
+        print1() <——— funcion
+         */
+        System.out.println("Hello world!");
+        int valor = 0; //es iniciada una variable para su futuro uso
+        System.out.println(valor);
+
+        //se declaran e inicializan 3 var double
+        double salario=0, iva=0.16, subtotal=10000;
+
+        final int cal_ap=70;
+        final String escuela="Tec II";
+
+        //error: solo se puede asignar una sola vez
+        //cal_ap=100;
+        //cal_ap=20;
+    }
+}
